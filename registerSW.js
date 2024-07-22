8fcaf1fb667cae4f26180b0630da0de4b2bd65b3
DrSemFrankenstein <DrSemFrankenstein>
2024-07-22 20:35:49 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Vote/sw.js', { scope: '/Vote/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
